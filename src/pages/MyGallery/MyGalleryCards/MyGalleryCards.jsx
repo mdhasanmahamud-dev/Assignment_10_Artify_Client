45ff88ab605dda6f132690b6d403ab61ecd33a21
MdHasanAliGHUN <mdhassanali01947@gmail.com>
2025-11-11 21:19:21 +0600

    empty state message in MyGalleryCards component

--- src/pages/MyGallery/MyGalleryCards/MyGalleryCards.jsx
+++ src/pages/MyGallery/MyGalleryCards/MyGalleryCards.jsx
@@ -4,10 +4,18 @@ import MyGalleryCard from "../MyGalleryCard/MyGalleryCard";
 const MyGalleryCards = ({ myGallery, galleryLoading }) => {
   if (galleryLoading) return <Spiner />;
   return (
-    <div className="container max-w-7xl mx-auto px-6 py-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-      {myGallery.map((gallery, index) => (
-        <MyGalleryCard key={index} gallery={gallery} />
-      ))}
+    <div className="container max-w-7xl mx-auto px-6 py-4">
+      {myGallery && myGallery.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {myGallery.map((gallery, index) => (
+            <MyGalleryCard key={index} gallery={gallery} />
+          ))}
+        </div>
+      ) : (
+        <p className="text-gray-100 font-semibold text-center">
+          No artworks found in your gallery.
+        </p>
+      )}
     </div>
   );
 };
